e4963220ec5436466f75cbc824515b41015df61b
Albert Sund Aillet <albert@sundaillet.com>
2022-04-29 15:10:14 +0200

    edited app to work with CLIPLSD

--- apps/2D_visualization.py
+++ apps/2D_visualization.py
@@ -3,15 +3,17 @@ import os
 import numpy as np
 import streamlit as st
 import torch
+import clip
 from PIL import ImageOps, Image, ImageFont, ImageDraw
 
 import sys
 
-sys.path.append("../")
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+  sys.path.append(module_path)
+
 import models
-from lelsd import LELSD
-from utils.biggan_utils import BigGANSampleGenerator
-from utils.stylegan1_utils import StyleGAN1SampleGenerator
+from cliplsd import CLIPLSD
 from utils.stylegan2_utils import StyleGAN2SampleGenerator
 
 model2available_dataset = {
@@ -42,24 +44,17 @@ def load_pretrained_model(model_name, dataset_name):
 
 
 @st.cache(ttl=None, allow_output_mutation=True, max_entries=2)
-def get_segmentation_model(model_name):
-    if model_name == "face_bisenet":
-        face_bisenet = models.get_model("face_bisenet", "../pretrained/face_bisenet/model.pth")
-        segmentation_model = FaceSegmentation(face_bisenet=face_bisenet, device=device)
-    elif model_name == 'deeplab':
-        deeplabv2_resnet101 = models.get_model("cocostuff_deeplab",
-                                               "../pretrained/cocostuff_deeplab/deeplabv2_resnet101_msc-cocostuff164k-100000.pth")
-        segmentation_model = StuffSegmentation(deeplabv2_resnet101=deeplabv2_resnet101, device=device)
+def get_clip_model(model_name):
+    if model_name in clip.available_models():
+        clip_model, _ = clip.load(model_name)
     else:
-        segmentation_model = None
-    return segmentation_model
+        clip_model = None
+    return clip_model
 
 
 @st.cache(ttl=None, allow_output_mutation=True, max_entries=1,
           hash_funcs={
-              StyleGAN1SampleGenerator: lambda s: hash(s.truncation_psi),
               StyleGAN2SampleGenerator: lambda s: hash(s.truncation_psi),
-              BigGANSampleGenerator: lambda s: hash(s.truncation_psi),
           })
 def get_batch_data(sample_generator, seed, model_name, dataset_name):
     batch_data = sample_generator.generate_batch(seed, return_image=True, return_style=True, batch_size=1)
@@ -68,30 +63,24 @@ def get_batch_data(sample_generator, seed, model_name, dataset_name):
 
 exp_dir = "../out/"
 
-stylegan1_part2lelsd_model_paths = {
-    #     "FFHQ": "lelsd_stylegan1_ffhq",
-    #     "WikiArt Faces": "lelsd_stylegan1_wikiart_faces",
-    #     "LSUN Bedroom": "lelsd_stylegan1_lsun_bedroom",
+stylegan2_part2cliplsd_model_paths = {
+    "FFHQ": "cliplsd_stylegan2_ffhq",
+    "LSUN Car": "cliplsd_stylegan2_lsun_car",
+    "MetFaces": "cliplsd_stylegan2_metfaces",
+    "LSUN Horse": "cliplsd_stylegan2_lsun_horse",
+    "LSUN Church": "cliplsd_stylegan2_lsun_church",
 }
 
-stylegan2_part2lelsd_model_paths = {
-    "FFHQ": "lelsd_stylegan2_ffhq",
-    "LSUN Car": "lelsd_stylegan2_lsun_car",
-    "MetFaces": "lelsd_stylegan2_metfaces",
-    "LSUN Horse": "lelsd_stylegan2_lsun_horse",
-    "LSUN Church": "lelsd_stylegan2_lsun_church",
+model2dataset2part2cliplsd = {
+#    "stylegan1": stylegan1_part2cliplsd_model_paths,
+    "stylegan2": stylegan2_part2cliplsd_model_paths,
 }
 
-model2dataset2part2lelsd = {
-    "stylegan1": stylegan1_part2lelsd_model_paths,
-    "stylegan2": stylegan2_part2lelsd_model_paths,
-}
-
-st.title("2D Visualization of LELSD")
+st.title("2D Visualization of CLIPLSD")
 
 model_name = st.sidebar.selectbox(
     "Choose the GAN type you want.",
-    ['stylegan1', 'stylegan2'],
+    ['stylegan2'],
 )
 
 dataset_name = st.sidebar.selectbox(
@@ -101,17 +90,19 @@ dataset_name = st.sidebar.selectbox(
 
 truncation_psi = st.sidebar.slider(f'Truncation Psi', 0.01, 1.0, 0.7)  # min, max, default
 
-model_path = os.path.join(exp_dir, model2dataset2part2lelsd[model_name][dataset_name])
+model_path = os.path.join(exp_dir, model2dataset2part2cliplsd[model_name][dataset_name])
 latent_space = st.sidebar.selectbox("Base Model Latent Space", os.listdir(model_path))
-last_path = os.path.join(model_path, latent_space, "2D")
-seg_type = st.sidebar.selectbox("Base Model Segmentation Model Type", os.listdir(last_path))
-last_path = os.path.join(last_path, seg_type)
-semantic_edit_part = st.sidebar.selectbox("Semantic Edit Part Name", os.listdir(last_path))
+last_path = os.path.join(model_path, latent_space)
+dim = st.sidebar.selectbox("Dimension", os.listdir(last_path))
+last_path = os.path.join(last_path, dim)
+clip_type = st.sidebar.selectbox("Clip Model Type", os.listdir(last_path))
+last_path = os.path.join(last_path, clip_type)
+semantic_edit_part = st.sidebar.selectbox("Semantic Text", os.listdir(last_path))
 last_path = os.path.join(last_path, semantic_edit_part)
 avaliable_dates = os.listdir(last_path)
 last_date = sorted(avaliable_dates, key=lambda x: datetime.datetime.strptime(x, '%b%d_%H-%M-%S'))[-1]
-base_lelsd_path = os.path.join(last_path, f"{last_date}/model.pth")
-base_lelsd = LELSD.load(base_lelsd_path)
+base_cliplsd_path = os.path.join(last_path, f"{last_date}/model.pth")
+base_cliplsd = CLIPLSD.load(base_cliplsd_path)
 
 alpha_range_type = st.sidebar.selectbox(
     "Choose the alpha range",
@@ -126,32 +117,30 @@ else:
     max_value = 75
     value = (-30, 30)
 
-if base_lelsd.latent_space.startswith("W"):
+if base_cliplsd.latent_space.startswith("W"):
     layers_to_apply = list(
-        map(int, st.sidebar.multiselect("Base LELSD Layers to apply the change", list(range(base_lelsd.n_layers)))))
-elif base_lelsd.latent_space.startswith("S"):
-    choices = base_lelsd.s_layers_to_apply
+        map(int, st.sidebar.multiselect("Base CLIPLSD Layers to apply the change", list(range(base_cliplsd.n_layers)))))
+elif base_cliplsd.latent_space.startswith("S"):
+    choices = base_cliplsd.s_layers_to_apply
     if choices:
-        layers_to_apply = list(map(int, st.sidebar.multiselect("Base LELSD Layers to apply the change", choices)))
+        layers_to_apply = list(map(int, st.sidebar.multiselect("Base CLIPLSD Layers to apply the change", choices)))
         if len(layers_to_apply) == 0:
             layers_to_apply = choices
     else:
         layers_to_apply = list(
             map(int,
-                st.sidebar.multiselect("Base LELSD Layers to apply the change", range(len(base_lelsd.latent_dirs)))))
+                st.sidebar.multiselect("Base CLIPLSD Layers to apply the change", range(len(base_cliplsd.latent_dirs)))))
 else:
     layers_to_apply = None
 
 G = load_pretrained_model(model_name, dataset_name)
 device = torch.device('cuda')
-if model_name == 'stylegan1':
-    sample_generator = StyleGAN1SampleGenerator(G=G, device=device, truncation_psi=truncation_psi)
-elif model_name == 'stylegan2':
+if model_name == 'stylegan2':
     sample_generator = StyleGAN2SampleGenerator(G=G, device=device, truncation_psi=truncation_psi)
 else:
     pass
 
-random_seed = int(st.sidebar.text_input('Random seed for generating the image', value='982'))
+random_seed = int(st.sidebar.text_input('Random seed for generating the image', value='1'))
 original_batch_data = get_batch_data(sample_generator, random_seed, model_name, dataset_name)
 original_image = original_batch_data['image'][0]
 original_raw_image = original_batch_data['raw_image']
@@ -161,7 +150,7 @@ width = 196
 height = 196
 img_size = (width, height)
 
-editing_model = base_lelsd
+editing_model = base_cliplsd
 latent_dir1_idx = st.sidebar.selectbox(
     "Choose the index of the first latent direction.",
     list(range(editing_model.num_latent_dirs))
@@ -197,4 +186,4 @@ for i in range(k):
         image = image.resize(img_size)
         editing_image[i * width: (i + 1) * width, j * height: (j + 1) * height, :] = np.array(image)
 
-st.image(editing_image, caption='2D Visualization of LELSD', use_column_width=False)
+st.image(editing_image, caption='2D Visualization of CLIPLSD', use_column_width=False)
